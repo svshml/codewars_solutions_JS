9ed32ae932fd096ffa6c61ecf5665073f0e26920
svshml <85778935+svshml@users.noreply.github.com>
2021-06-12 11:23:19 -0400

    Update InterweavingStringsAndRemovingDigits.js

--- InterweavingStringsAndRemovingDigits.js
+++ InterweavingStringsAndRemovingDigits.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/588a7d45019c42be61000009
-
+/*
 Your friend Rick is trying to send you a message, but he is concerned that it would get intercepted by his partner. He came up with a solution:
 
 Add digits in random places within the message.
@@ -11,7 +11,7 @@ Write a function interweave(s1, s2) that reverses this operation to decode his m
 Example 1: interweave("hlo", "el") -> "hello" Example 2: interweave("h3lo", "el4") -> "hello"
 
 Rick's a bit peculiar about his formats. He would feel ashamed if he found out his message led to extra white spaces hanging around the edges of his message...
-
+*/
 
 function interweave(s1, s2) {
   s1 = s1.split('')
