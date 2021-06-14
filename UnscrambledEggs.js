1ee23168ed85b2e28f412ed0e825cf57aa8038e5
svshml <85778935+svshml@users.noreply.github.com>
2021-06-14 09:49:46 -0400

    Create UnscrambledEggs.js

---
+++ UnscrambledEggs.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/55ea5650fe9247a2ea0000a7
+The string given to your function has had an "egg" inserted directly after each consonant. You need to return the string before it became eggcoded.
+
+Example
+unscrambleEggs("Beggegeggineggneggeregg"); => "Beginner"
+//             "B---eg---in---n---er---"
+*/
+
+function unscrambleEggs(word){
+return word.replace(/egg/gi, '');
+}
