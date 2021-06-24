869ed1501ffcde6178be75ddb7b9bfd7677121cc
svshml <85778935+svshml@users.noreply.github.com>
2021-06-24 12:36:29 -0400

    Create StringRepeat.js

---
+++ StringRepeat.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+
+Write a function called repeatStr which repeats the given string string exactly n times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+*/
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
