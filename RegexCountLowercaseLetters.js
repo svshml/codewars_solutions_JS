0d1b65c12f4ee5cc6e857dfad828fe99869aa253
svshml <85778935+svshml@users.noreply.github.com>
2021-06-28 09:40:34 -0400

    Create RegexCountLowercaseLetters.js

---
+++ RegexCountLowercaseLetters.js
@@ -0,0 +1,24 @@
+/*
+https://www.codewars.com/kata/56a946cd7bd95ccab2000055
+
+Your task is simply to count the total number of lowercase letters in a string.
+
+Examples
+lowercaseCount("abc"); ===> 3
+
+lowercaseCount("abcABC123"); ===> 3
+
+lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 3
+
+lowercaseCount(""); ===> 0;
+
+lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 0
+
+lowercaseCount("abcdefghijklmnopqrstuvwxyz"); ===> 26
+
+*/
+
+function lowercaseCount(str){
+  let Althabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  return str.split('').reduce((count,el)=> count + Althabet.includes(el), 0);
+}
