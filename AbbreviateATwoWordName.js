812f1640ac6321d2bc0b44a3c488044bc223175e
svshml <85778935+svshml@users.noreply.github.com>
2021-06-29 15:38:48 -0400

    Create AbbreviateATwoWordName.js

---
+++ AbbreviateATwoWordName.js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
+
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+Patrick Feeney => P.F 
+
+*/
+
+function abbrevName(name){
+ let arr =name.split(' ');
+  return arr[0][0].toUpperCase()+'.' + arr[1][0].toUpperCase();
+  }
