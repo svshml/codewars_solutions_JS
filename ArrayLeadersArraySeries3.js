b831f6a9053e6cc53774edc728a6ea641d578f59
svshml <85778935+svshml@users.noreply.github.com>
2021-07-04 10:28:55 -0400

    Create ArrayLeadersArraySeries3.js

---
+++ ArrayLeadersArraySeries3.js
@@ -0,0 +1,34 @@
+/*
+https://www.codewars.com/kata/5a651865fd56cb55760000e0
+Introduction and Warm-up (Highly recommended)
+Playing With Lists/Arrays Series
+Definition
+An element is leader if it is greater than The Sum all the elements to its right side.
+
+Task
+Given an array/list [] of integers , Find all the LEADERS in the array.
+
+Notes
+Array/list size is at least 3 .
+Array/list's numbers Will be mixture of positives , negatives and zeros
+Repetition of numbers in the array/list could occur.
+Returned Array/list should store the leading numbers in the same order in the original array/list .
+
+Input >> Output Examples
+arrayLeaders ({1, 2, 3, 4, 0}) ==> return {4}
+Explanation:
+4 is greater than the sum all the elements to its right side
+
+Note : The last element 0 is equal to right sum of its elements (abstract zero).
+*/
+
+
+var arrayLeaders = numbers => {
+  let newArr=[];
+  for (i = 0; i < numbers.length; i++){
+   if ( numbers.filter( (el,j) => j > i).reduce((sum,elm) => sum + elm,0) < numbers[i]){
+   newArr.push(numbers[i]);
+     }
+}
+return newArr;
+}
