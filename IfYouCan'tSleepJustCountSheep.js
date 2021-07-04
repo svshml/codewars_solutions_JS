53ce65fdc52815488459a62be2843423dd30db6e
svshml <85778935+svshml@users.noreply.github.com>
2021-07-04 10:16:59 -0400

    Create IfYouCan'tSleepJustCountSheep.js

---
+++ IfYouCan'tSleepJustCountSheep.js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+
+If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". 
+Input will always be valid, i.e. no negative integers.
+*/
+
+var countSheep = function (num){
+  let result ='';
+let i = 1;  
+while (i <= num) {
+  result+= `${i} sheep...`;
+  i++;
+}
+return result;
+  }
