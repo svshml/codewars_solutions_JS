bb212fb952a4fd1b89ece8bcd1b42b762c290ad2
svshml <85778935+svshml@users.noreply.github.com>
2021-07-04 10:20:53 -0400

    Create GrasshopperVariableAssignmentDebug.js

---
+++ GrasshopperVariableAssignmentDebug.js
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/5612e743cab69fec6d000077
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+*/
+var a = "dev"
+var b = "Lab"
+
+var name = a + b;
+console.log(a+b);
