dc826e43bdb659daf5de01c692d9582ce28092ba
svshml <85778935+svshml@users.noreply.github.com>
2021-07-12 13:27:51 -0400

    Create TripleTrouble.js

---
+++ TripleTrouble.js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/5704aea738428f4d30000914
+
+Create a function that will return a string that combines all of the letters of the three inputed strings in groups. 
+Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+Note: You can expect all of the inputs to be the same length.
+*/
+
+
+function tripleTrouble(one, two, three){
+  let str ='';
+   for (i=0; i < one.length ;i++){
+    str =str + one[i] + two[i]+ three[i];
+  }
+  return str;
+ }
