e4662f36dff405481d446e7c884a1d872cd30498
svshml <85778935+svshml@users.noreply.github.com>
2021-08-05 12:50:38 -0400

    Create  SumOfDifferencesInArray.js

---
+++ SumOfDifferencesInArray.js
@@ -0,0 +1,26 @@
+/*
+https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e
+
+Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+For example:
+
+sumOfDifferences([2, 1, 10])
+Returns 9
+
+Descending order: [10, 2, 1]
+
+Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell).
+*/
+
+function sumOfDifferences(arr) {
+  arr = arr.sort((a,b) => b-a);
+  result =0;
+  for (i =0; i+1 < arr.length; i++){
+    result+= arr[i] -arr[i+1];
+  }
+return result;  
+}
+
