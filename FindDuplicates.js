cffff4635cff8bc48a68198c44c41ea1ac8c100a
svshml <85778935+svshml@users.noreply.github.com>
2021-08-09 13:32:25 -0400

    Create FindDuplicates.js

---
+++ FindDuplicates.js
@@ -0,0 +1,25 @@
+/*
+https://www.codewars.com/kata/5558cc216a7a231ac9000022
+
+Given an array, find the duplicates in that array, and return a new array of those duplicates. The elements of the returned array 
+should appear in the order when they first appeared as duplicates.
+
+Note: numbers and their corresponding string representations should not be treated as duplicates (i.e., "1" != 1).
+
+Examples
+[1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]  ==>  [4, 3, 1]
+[0, 1, 2, 3, 4, 5]                ==>  []
+*/
+
+function duplicates(arr) {
+  let newArr =[];
+  for(i = 0; i <arr.length; i++){
+    for ( j= 0; j < i;j++){
+      if (arr[i] === arr[j] && i != j && !newArr.includes(arr[i]))
+        newArr.push(arr[i]);
+    }
+  }
+  
+return newArr;
+}
+
