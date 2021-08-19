ee84a9746d7a5354c84bec4d08178893aa3a0aee
svshml <85778935+svshml@users.noreply.github.com>
2021-08-19 08:13:52 -0400

    Create  GetTheIntegersBetweenTwoNumbers.js

---
+++ GetTheIntegersBetweenTwoNumbers.js
@@ -0,0 +1,23 @@
+/*
+https://www.codewars.com/kata/598057c8d95a04f33f00004e
+Build a function that can get all the integers between two given numbers.
+
+Example:
+
+(2,9)
+
+Should give you this output back:
+
+[ 3, 4, 5, 6, 7, 8 ]
+
+If startNum is the same as endNum, return an empty array.
+*/
+
+function range(startNum, endNum){  
+  let arr=[];
+ for (let i = startNum+1; i < endNum;i++){
+ arr.push(i)
+ };
+ return arr;
+}
+
