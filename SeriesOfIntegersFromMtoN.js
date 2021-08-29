1315d811ca1eedbfe6b51fa6d53e22a8cd917344
svshml <85778935+svshml@users.noreply.github.com>
2021-08-28 20:56:20 -0400

    Create SeriesOfIntegersFromMtoN.js

---
+++ SeriesOfIntegersFromMtoN.js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/5841f680c5c9b092950001ae
+Task
+Write a function that accepts two arguments and generates a sequence containing the integers from the first argument to the second inclusive.
+
+Input
+Pair of integers greater than or equal to 0. The second argument will always be greater than or equal to the first.
+
+Example
+generateIntegers(2, 5) // --> [2, 3, 4, 5]
+*/
+
+function generateIntegers(m, n) {
+ let arr=[];
+for ( i = m ; i <= n; i++){
+arr.push(i);
+}
+return arr; 
+}
