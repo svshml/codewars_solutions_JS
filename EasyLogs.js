e6be263653fa5c9f93799998ce06ac6eb9da245c
svshml <85778935+svshml@users.noreply.github.com>
2021-09-09 17:44:44 -0400

    Create EasyLogs.js

---
+++ EasyLogs.js
@@ -0,0 +1,7 @@
+/*
+https://www.codewars.com/kata/5b68c7029756802aa2000176
+Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+*/
+function logs(x , a, b){
+return (Math.log(a) + Math.log(b)) / Math.log(x);
+}
