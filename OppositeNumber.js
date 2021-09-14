a38a2f6c7f4311fce57b0a261b5dc47e910dbca3
svshml <85778935+svshml@users.noreply.github.com>
2021-09-13 21:33:07 -0400

    Create  OppositeNumber.js

---
+++ OppositeNumber.js
@@ -0,0 +1,15 @@
+/*
+https://www.codewars.com/kata/56dec885c54a926dcd001095
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+*/
+
+function opposite(number) {
+ return -number;
+}
