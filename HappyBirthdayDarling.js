a4002731ef86e184ece490cb9ce1d717d9e55f39
svshml <85778935+svshml@users.noreply.github.com>
2021-10-02 15:25:52 -0400

    Create HappyBirthdayDarling.js

---
+++ HappyBirthdayDarling.js
@@ -0,0 +1,26 @@
+/*
+https://www.codewars.com/kata/5e96332d18ac870032eb735f
+As you may know, once some people pass their teens, they jokingly only celebrate their 20th or 21st birthday, forever. 
+With some maths skills, that's totally possible - you only need to select the correct number base!
+
+For example, if they turn 32, that's exactly 20 - in base 16... Already 39? That's just 21, in base 19!
+
+Your task is to translate the given age to the much desired 20 (or 21) years, and indicate the number base, in the format specified below.
+
+Note: input will be always > 21
+
+Examples:
+32  -->  "32? That's just 20, in base 16!"
+39  -->  "39? That's just 21, in base 19!"
+*/
+
+function womensAge(n) { 
+ if (n % 2 === 0) {
+   let base=n/2;
+    return `${n}? That's just 20, in base ${base}!` ;
+  }
+  if ((n -1)% 2 === 0) {
+   let base=(n-1)/2;
+    return `${n}? That's just 21, in base ${base}!` ;
+  }
+}
