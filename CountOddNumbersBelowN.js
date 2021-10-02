f290a58ed650e1fa25e789f324efb3de4cdd48ed
svshml <85778935+svshml@users.noreply.github.com>
2021-10-02 15:24:15 -0400

    Create CountOddNumbersBelowN.js

---
+++ CountOddNumbersBelowN.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/59342039eb450e39970000a6
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+oddCount(7) //=> 3, i.e [1, 3, 5]
+oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+Expect large Inputs!
+*/
+
+function oddCount(n){
+  return Math.floor(n/2);
+}
