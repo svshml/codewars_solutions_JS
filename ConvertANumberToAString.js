ad0887cc1f0e7eba78f3ef5187b758ed355c1153
svshml <85778935+svshml@users.noreply.github.com>
2021-10-03 12:17:27 -0400

    Create ConvertANumberToAString.js

---
+++ ConvertANumberToAString.js
@@ -0,0 +1,14 @@
+/*
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+123 --> "123"
+999 --> "999"
+*/
+
+function numberToString(num) {
+ return String(num);
+}
