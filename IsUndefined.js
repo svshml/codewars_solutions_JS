5e1e11953b7735e113cedeb2371178b9f6e6c05c
svshml <85778935+svshml@users.noreply.github.com>
2021-10-04 11:39:23 -0400

    Create  IsUndefined.js

---
+++ IsUndefined.js
@@ -0,0 +1,7 @@
+/*
+https://www.codewars.com/kata/564a6486c2070faee6000011
+Your task is to finish the isUndefined method which returns true if the value is undefined
+*/
+function isUndefined(value) {
+  return typeof(value) === 'undefined' ;
+}
