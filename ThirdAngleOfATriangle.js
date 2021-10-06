d5676d3d208d470325095850fa3331458fdb3323
svshml <85778935+svshml@users.noreply.github.com>
2021-10-06 08:12:44 -0400

    Create ThirdAngleOfATriangle.js

---
+++ ThirdAngleOfATriangle.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/5a023c426975981341000014
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+*/
+
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
