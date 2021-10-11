a7701adf545b8d35e5014d39919b9ba8d6215396
svshml <85778935+svshml@users.noreply.github.com>
2021-10-10 21:09:40 -0400

    Create GrasshopperCreateTheRooms.js

---
+++ GrasshopperCreateTheRooms.js
@@ -0,0 +1,27 @@
+/*
+https://www.codewars.com/kata/56a29b237e9e997ff2000048
+Escape the room
+You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. 
+There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+*/
+
+var rooms = {
+  room1:{
+  name: "Room №1",
+  description: "Mystery Lock.",
+  completed: 31,
+  },
+  
+  room2:{
+  name: "Room №2",
+  description: "Clue Breakout.",
+  completed: 42,
+  },
+  
+  room3:{
+  name: "Room №3",
+  description: "The Vault Mystery.",
+  completed: 53,
+  },
+
+}
