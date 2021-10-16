c76b363ae2336cf4486e67c37859415db5b646b8
svshml <85778935+svshml@users.noreply.github.com>
2021-10-16 11:24:13 -0400

    Update ABugsTrilogyEpisode1.js

--- ABugsTrilogyEpisode1.js
+++ ABugsTrilogyEpisode1.js
@@ -22,3 +22,8 @@ function yourFutureCareer(){
      else 
       return 'Full-Stack Developer';
  }
+
+const yourFutureCareer = () => {
+  let career = Math.random();
+  return `${ career <= 0.32 ? 'FrontEnd' : (career <= 0.65 ? 'BackEnd' : 'Full-Stack') } Developer`;
+}
