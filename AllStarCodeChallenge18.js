7a1e7082d07571c1815208a870bcbef3c61f8b55
svshml <85778935+svshml@users.noreply.github.com>
2021-10-28 21:48:35 -0400

    Update and rename AllStarCodeChallenge.js to AllStarCodeChallenge18.js

--- AllStarCodeChallenge18.js
+++ AllStarCodeChallenge18.js
@@ -17,3 +17,13 @@ function strCount(str, letter){
  return str.split('').filter(el=> el ==letter).length;
 }
 
+
+function strCount(str, letter){  
+let count =0;
+ for (let i=0; i< str.length;i++){
+   if (str[i] == letter)
+     count++;
+ } 
+  return count;
+}
+
