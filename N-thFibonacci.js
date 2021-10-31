0df29940228d6d05edc9507e29c71784cf298438
svshml <85778935+svshml@users.noreply.github.com>
2021-10-31 13:49:39 -0400

    Create N-thFibonacci.js

---
+++ N-thFibonacci.js
@@ -0,0 +1,21 @@
+/*
+https://www.codewars.com/kata/522551eee9abb932420004a0
+I love Fibonacci numbers in general, but I must admit I love some more than others.
+
+I would like for you to write me a function that when given a number (n) returns the n-th number in the Fibonacci Sequence.
+
+For example:
+
+   nthFibo(4) == 2
+Because 2 is the 4th number in the Fibonacci Sequence.
+
+For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.
+*/
+
+function nthFibo(n) {
+  if (n == 1) return 0;
+  if (n == 2) return 1;
+
+  return nthFibo(n-1) + nthFibo(n-2);
+  
+}
