8f08348815761b8f4b5160ea1a5abc9a01ef44e0
svshml <85778935+svshml@users.noreply.github.com>
2021-11-06 11:32:57 -0400

    Create ElevatorDistance.js

---
+++ ElevatorDistance.js
@@ -0,0 +1,26 @@
+/*
+https://www.codewars.com/kata/59f061773e532d0c87000d16
+Imagine you start on the 5th floor of a building, then travel down to the 2nd floor, then back up to the 8th floor. 
+You have travelled a total of 3 + 6 = 9 floors of distance.
+
+Given an array representing a series of floors you must reach by elevator, return an integer representing 
+the total distance travelled for visiting each floor in the array in order.
+
+// simple examples
+elevatorDistance([5,2,8]) = 9
+elevatorDistance([1,2,3]) = 2
+elevatorDistance([7,1,7,1]) = 18
+
+// if two consecutive floors are the same,
+//distance travelled between them is 0
+elevatorDistance([3,3]) = 0
+*/
+
+function elevatorDistance(array) {
+let res = 0;
+ for(let i=1; i < array.length;i++){
+  res+= Math.abs(array[i] - array[i-1]);  
+ } 
+ return res;
+
+}
