bc08870c451c50dd43cfff068e27ba5ba73f7321
svshml <85778935+svshml@users.noreply.github.com>
2021-11-30 10:02:27 -0500

    Create SpecialNumberSpecialNumbersSeries5.js

---
+++ SpecialNumberSpecialNumbersSeries5.js
@@ -0,0 +1,48 @@
+/*
+https://www.codewars.com/kata/5a55f04be6be383a50000187
+Definition
+A number is a Special Number if it’s digits only consist 0, 1, 2, 3, 4 or 5
+
+Given a number determine if it special number or not .
+
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+The number passed will be positive (N > 0) .
+
+All single-digit numbers with in the interval [0:5] are considered as special number.
+
+Input >> Output Examples
+specialNumber(2) ==> return "Special!!"
+Explanation:
+It's a single-digit number within the interval [0:5] .
+
+specialNumber(9) ==> return "NOT!!"
+Explanation:
+Although, it's a single-digit number but Outside the interval [0:5] .
+
+specialNumber(23) ==> return "Special!!"
+Explanation:
+All the number's digits formed from the interval [0:5] digits .
+
+specialNumber(39) ==> return "NOT!!"
+Explanation:
+Although, there is a digit (3) Within the interval But the second digit is not (Must be ALL The Number's Digits ) .
+
+specialNumber(59) ==> return "NOT!!"
+Explanation:
+Although, there is a digit (5) Within the interval But the second digit is not (Must be ALL The Number's Digits ) .
+
+specialNumber(513) ==> return "Special!!"
+specialNumber(709) ==> return "NOT!!"
+*/
+
+function specialNumber(n){
+ let newN = n.toString(); 
+ let special = '123450';
+ for(let i =0;i < newN.length;i++){
+  if (special.includes(newN[i]) == false)
+    return "NOT!!"
+ } 
+  return "Special!!";
+}
