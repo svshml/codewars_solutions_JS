a1c92373ea41023052e6b40f0a37b7c34df23b80
svshml <85778935+svshml@users.noreply.github.com>
2021-12-02 12:31:40 -0500

    Create ZipWith.js

---
+++ ZipWith.js
@@ -0,0 +1,27 @@
+/*
+https://www.codewars.com/kata/5825792ada030e9601000782
+
+zipWith takes a function and two arrays and zips the arrays together, applying the function to every pair of values.
+The function value is one new array.
+
+If the arrays are of unequal length, the output will only be as long as the shorter one.
+(Values of the longer array are simply not used.)
+
+Inputs should not be modified.
+
+Examples
+zipWith( Math.pow, [10,10,10,10], [0,1,2,3] )      =>  [1,10,100,1000]
+zipWith( Math.max, [1,4,7,1,4,7], [4,7,1,4,7,1] )  =>  [4,7,7,4,7,7]
+
+zipWith( function(a,b) { return a+b; }, [0,1,2,3], [0,1,2,3] )  =>  [0,2,4,6]  Both forms are valid.
+zipWith( (a,b) => a+b,                  [0,1,2,3], [0,1,2,3] )  =>  [0,2,4,6]  Both are functions.
+Input validation
+Assume all input is valid.
+*/
+
+function zipWith(fn,a0,a1) {
+  if(a1.length < a0.length)
+    return a1.map((el,i)=> fn(a0[i],el))
+  else 
+    return a0.map((el,i)=> fn(el,a1[i]));  
+}
