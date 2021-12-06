f71749edcfef80448f4be5cd86403aa0f85a21c7
svshml <85778935+svshml@users.noreply.github.com>
2021-12-06 18:59:08 -0500

    Create WhatComesAfter.js

---
+++ WhatComesAfter.js
@@ -0,0 +1,23 @@
+/*
+https://www.codewars.com/kata/590f5b4a7bbb3e246000007d
+You will be given two inputs: a string of words and a letter. For each string, return the alphabetic character after every instance of letter(case insensitive).
+
+If there is a number, punctuation or underscore following the letter, it should not be returned.
+
+If letter = 'r':
+comes_after("are you really learning Ruby?") # => "eenu"
+comes_after("Katy Perry is on the radio!")   # => "rya"
+comes_after("Pirates say arrrrrrrrr.")       # => "arrrrrrrr"
+comes_after("r8 your friend")                # => "i"
+Return an empty string if there are no instances of letter in the given string.
+
+
+*/
+function comes_after(str,l) {
+let result = '';
+  for (let i=0; i < str.length-1; i++){
+   if (str[i].toLowerCase() == l.toLowerCase() && str[i+1].match(/^[a-z]+$/i))
+    result = result + str[i+1];    
+ }
+  return result;
+}
