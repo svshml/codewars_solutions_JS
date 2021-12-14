bec6f0b1f0c30b92e235ee8672744c5b95ade56e
svshml <85778935+svshml@users.noreply.github.com>
2021-12-14 18:04:30 -0500

    Create SubstringFun.js

---
+++ SubstringFun.js
@@ -0,0 +1,25 @@
+/*
+https://www.codewars.com/kata/565b112d09c1adfdd500019c
+
+Complete the function that takes an array of words.
+
+You must concatenate the nth letter from each word to construct a new word which should be returned as a string, where n is the position of the word in the list.
+
+For example:
+
+["yoda", "best", "has"]  -->  "yes"
+  ^        ^        ^
+  n=0     n=1     n=2
+Note: Test cases contain valid input only - i.e. a string array or an empty array; and each word will have enough letters.
+*/
+
+function nthChar(words){
+  let result = '';
+  for(let i = 0; i< words.length; i++){
+    for(let j = 0; j< words.length; j++){
+      if (i ==j)
+       result+= words[i][j]; 
+     }
+   }
+ return result;
+}
