a9314d9a3b8c66ddf2e4937bfb26c29f78aded2b
Lana Shamal <85778935+svshml@users.noreply.github.com>
2022-04-21 08:49:52 -0400

    Create ThinkfulListDrillsLongestWord.js

---
+++ ThinkfulListDrillsLongestWord.js
@@ -0,0 +1,13 @@
+/*
+https://www.codewars.com/kata/58670300f04e7449290000e5
+Complete the function that takes one argument, a list of words, and returns the length of the longest word in the list.
+
+For example:
+
+['simple', 'is', 'better', 'than', 'complex'] ==> 7
+Do not modify the input list.
+*/
+
+function longest(words) {
+  return Math.max(...words.map(el => el.length )); 
+}
